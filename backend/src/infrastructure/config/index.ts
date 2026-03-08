e02354e680fe6c4ae3ae50b3206bb9451205aee3
Vlada <vlada.shevchenko.it@gmail.com>
2026-03-08 18:22:03 +0000

    chore(backend): add swagger configuration

--- backend/src/infrastructure/config/index.ts
+++ backend/src/infrastructure/config/index.ts
@@ -1,2 +1,3 @@
 export * from './validation-pipe.config';
 export * from './core.config';
+export * from './swagger.config';
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { Logger } from '@nestjs/common/services/logger.service';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule } from '@nestjs/swagger';
 import {
   getCoreConfig,
   getValidationPipeConfig,
+  swaggerConfig,
 } from './infrastructure/config';
-import { Logger } from '@nestjs/common/services/logger.service';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,11 +19,17 @@ async function bootstrap() {
 
   app.enableCors(getCoreConfig(config));
 
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, swaggerDocument, {
+    yamlDocumentUrl: '/openapi.yaml',
+  });
+
   const port = config.getOrThrow<number>('HTTP_PORT');
   const host = config.getOrThrow<string>('HTTP_HOST');
 
   await app.listen(port);
 
   logger.log(`Gateway started: ${host}`);
+  logger.log(`Swagger: ${host}/docs`);
 }
-bootstrap();
+void bootstrap();
