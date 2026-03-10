897fc5c34df7484e014e26239f6bd386d36e8312
Vlada <vlada.shevchenko.it@gmail.com>
2026-03-10 01:43:00 +0000

    feat(frontend/ui): add reusable Button component

---
+++ frontend/src/components/ui/button/button-types.ts
@@ -0,0 +1,7 @@
+export const BUTTON_VARIANTS = {
+  primary: 'bg-indigo-500 text-white hover:bg-indigo-600',
+  join: 'bg-green-600 text-white hover:bg-green-700',
+  lightBorder:
+    'bg-white text-gray-800 hover:bg-indigo-100 border border-gray-300',
+  light: 'bg-white hover:bg-indigo-100',
+} as const
