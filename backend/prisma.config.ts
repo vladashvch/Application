ed8504ee665d1760a162dd94608e98e9952a9b6f
Vlada <vlada.shevchenko.it@gmail.com>
2026-03-12 10:24:51 +0000

    feat(docker): add app containerization and docker-compose

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,13 +1,13 @@
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import 'dotenv/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env['DATABASE_URL'],
   },
 });
